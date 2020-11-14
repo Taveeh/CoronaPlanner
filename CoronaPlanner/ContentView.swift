78e19a8c9495c454856441d88bec26ebabd1b0ba
Taveeh <octavian.custura@gmail.com>
2020-11-14 14:07:25 +0200

    Initial Commit

---
+++ CoronaPlanner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoronaPlanner
+//
+//  Created by Octavian Custura on 14/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
